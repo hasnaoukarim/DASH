217e3624caec1bb18465f9dad7b93319f03a534b
hasnaoukarim <hasnaoukarim@gmail.com>
2021-03-19 15:47:08 +0100

    this is the first change

---
+++ TEST/test2.py
@@ -0,0 +1,18 @@
+#start test
+
+
+def fname(arg):
+    pass
+
+    """this is a test"""
+
+
+#new modification
+class ClassName(object):
+    """docstring for ."""
+
+    def __init__(self, arg):
+        super(, self).__init__()
+        self.arg = arg
+
+"""this is a new change"""
